8830764e1f1e57b0768d8772b241bb0c85fabad8
David Hu <davidhu91@gmail.com>
2015-07-31 17:14:10 -0700

    add brush strokes w/ inking

--- paper.js
+++ paper.js
@@ -3,20 +3,21 @@ var drawingData = window.drawingData1;
 var path = new Path({
   strokeColor: 'rgb(213, 129, 174)',
   strokeWidth: '5',
-  strokeCap: 'miter',
+  strokeCap: 'round',
 });
 
 var pointIndex = 0;
 var firstPointTime = drawingData[0][2];
 
-////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 // Main animation loop
 
 function onFrame(event) {
   var currentTime = event.time;
   var timeOffset = currentTime + firstPointTime;
 
-  for (; drawingData[pointIndex][2] < timeOffset; pointIndex++) {
+  for (; drawingData[pointIndex] &&
+       drawingData[pointIndex][2] < timeOffset; pointIndex++) {
     var datum = drawingData[pointIndex];
     var x = datum[0];
     var y = datum[1] - 400;
@@ -27,7 +28,44 @@ function onFrame(event) {
   path.smooth();
 }
 
-////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+// Brush tool
+
+tool.minDistance = 8;
+tool.maxDistance = 40;
+var brushPath;
+
+function onMouseDown(event) {
+  brushPath = new Path({
+    fillColor: {
+      hue: Math.random() * 360,
+      saturation: 0.8,
+      brightness: 0.7
+    }
+  });
+  brushPath.add(event.point);
+}
+
+function onMouseDrag(event) {
+  var step = event.delta;
+  step.angle += 90;
+  step.length = (1 / step.length) * 15;
+
+  var top = event.middlePoint + step;
+  var bottom = event.middlePoint - step;
+
+  brushPath.add(top);
+  brushPath.insert(0, bottom);
+  brushPath.smooth();
+}
+
+function onMouseUp(event) {
+  brushPath.add(event.point);
+  brushPath.closed = true;
+  brushPath.smooth();
+}
+
+///////////////////////////////////////////////////////////////////////////////
 // Event handlers
 
 $('#zoom-in-btn').on('click', function() {
